caca2dc865ea5e300643b9b04a24604d8d9235cb
Cameron Moreau <cameron.cm6@gmail.com>
2015-03-05 17:00:03 -0600

    added submitted page and cookies.. kind of

--- public/javascripts/load.js
+++ public/javascripts/load.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
 	$('#header-text').animate({marginTop: '0'}, 500);
+	$('#logo').animate({marginTop: '0'}, 500);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-  	title: 'Mobi Sticker Challenge'
+
+	if(req.cookies.submitted !== undefined) {
+		res.redirect('/submit');
+	}
+
+	res.render('index', { 
+	  	title: 'Mobi Sticker Challenge'
+	  });
+	
+});
+
+router.post('/submit', function(req, res, next) {
+  //res.send(req.body);
+  var firstName = req.body.first_name.replace(/(<([^>]+)>)/ig,"");
+  var lastName = req.body.last_name.replace(/(<([^>]+)>)/ig,"");
+  var fact = req.body.fact.replace(/(<([^>]+)>)/ig,"");
+
+  //Expires in a day
+  res.cookie('submitted', firstName, { expires: new Date(Date.now() + 86400) });
+
+  res.render('submit', { 
+  	name: req.cookies.submitted
   });
 });
 
