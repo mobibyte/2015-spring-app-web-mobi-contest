d04d75b37eba88a9404f3401e768a71d38106a7d
Cameron Moreau <cameron.cm6@gmail.com>
2015-03-05 20:53:57 -0600

    Finished cookies, checks if user is on mobile

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,63 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+	var userAgent = req.headers['user-agent'];
+
 	if(req.cookies.submitted !== undefined) {
 		res.redirect('/submit');
 	}
 
+	var isMobile = {
+    Android: function() {
+        return userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 	res.render('index', { 
-	  	title: 'Mobi Sticker Challenge'
+	  	title: 'Mobi Sticker Challenge',
+	  	onMobileDevice: isMobile.any()
 	  });
 	
 });
 
-router.post('/submit', function(req, res, next) {
-  //res.send(req.body);
+router.post('/submit', function(req, res) {
+	//Sanitize data
   var firstName = req.body.first_name.replace(/(<([^>]+)>)/ig,"");
   var lastName = req.body.last_name.replace(/(<([^>]+)>)/ig,"");
   var fact = req.body.fact.replace(/(<([^>]+)>)/ig,"");
 
-  //Expires in a day
-  res.cookie('submitted', firstName, { expires: new Date(Date.now() + 86400) });
+  //Validate data
+  if(firstName != '' && lastName != '' && fact != '') {
 
-  res.render('submit', { 
+  	//Set cookie which expires in 24 hours
+	  res.cookie('submitted', firstName, { expires: new Date(Date.now() + 86400) });
+	  res.redirect('/submit');
+  } else {
+  	res.send('All forms are required');
+  }
+});
+
+router.get('/submit', function(req, res) {
+	if(req.cookies.submitted == undefined) {
+		res.redirect('/');
+	}
+
+	res.render('submit', { 
   	name: req.cookies.submitted
   });
 });
